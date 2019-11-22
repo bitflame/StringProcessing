dc812ed638149c27d3d72b72bb21ec73666046ef
Liquid Falme <sansari1@cox.net>
2019-11-22 16:54:04 -0500

    First Commit. Need to skip it instead of printing it.

---
+++ StringProcessing/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringProcessing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string testString = "I really need to <learn> this.";
+            MyStringProcessor(testString);
+        }
+        private static void MyStringProcessor (string s)
+        {
+            int pFrom = s.IndexOf("<");
+            int pTo = s.LastIndexOf(">");
+
+            String result = s.Substring(pFrom + 1, pTo - pFrom -1);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
